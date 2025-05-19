40aab647965a0eed65b2b1c4e2955edd27e5c568
Anthony Miyaguchi <acmiyaguchi@gatech.edu>
2025-05-19 03:12:31 +0000

    Add initial submission system

---
+++ submission/README.md
@@ -0,0 +1,37 @@
+# submission
+
+This is a simple proxy to the rest of the services.
+
+# How to run
+
+To compile dependencies:
+
+```bash
+uv pip compile pyproject.toml > requirements.txt
+```
+
+To run the services:
+
+```bash
+docker compose build
+docker compose up
+```
+
+Do the tira things:
+
+```bash
+uv tool install tira
+```
+
+Run the `tira-cli login` command as per the instructions in the tira submission page for docker.
+
+```bash
+tira-cli code-submission \
+    --path . \
+    --task retrieval-augmented-debating-2025 \
+    --command '/genirsim/run.sh         --configuration-file=$inputDataset/*.json --parameter-file=$inputDataset/*.tsv --output-file=$outputDir/simulations.jsonl' \
+    --allow-network \
+    --dry-run
+```
+
+This is not quite the docker submission, but we're not doing anything more complex than the default submission if we can assume that requests is in the environment.
---
+++ submission/main.py
@@ -0,0 +1,73 @@
+from elasticsearch import Elasticsearch
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
+client = Elasticsearch("https://touche25-rad.webis.de/arguments")
+index = "claimrev"
+
+
+class Message(BaseModel):
+    role: str
+    content: str
+
+
+class Request(BaseModel):
+    messages: List[Message]
+
+
+def reply(messages: List[Message]):
+    """
+    Continues the conversation.
+
+    Args:
+        messages: The conversation so far
+
+    Returns:
+        str: The response text
+        List: The list of arguments that were used to generate that response, each an object with at least the collection "id"
+    """
+    claim = messages[-1].content
+    top_result = next(query_elastic(claim))
+    return top_result["text"], [top_result]
+
+
+def query_elastic(claim, size=1):
+    """
+    Simple function to query the RAD Elasticsearch server.
+
+    Args:
+      claim: The claim to be rebutted
+      size: The amount of results to retrieve
+
+    Returns:
+      Generator of result objects
+    """
+    # see https://elasticsearch-py.readthedocs.io/en/v8.17.0/api/elasticsearch.html#elasticsearch.client.Elasticsearch.search
+    response = client.search(
+        index=index,
+        query={"match": {"attacks": {"query": claim}}},
+        source_excludes=[
+            "text_embedding_stella",
+            "supports_embedding_stella",
+            "attacks_embedding_stella",
+        ],
+        size=size,
+    )
+    rank = 1
+    for hit in response["hits"]["hits"]:
+        result = hit["_source"]
+        result["key"] = rank
+        result["id"] = hit["_id"]
+        result["score"] = hit["_score"]
+        rank += 1
+        yield result
+
+
+app = FastAPI()
+
+
+@app.post("/")
+async def respond(request: Request):
+    content, arguments = reply(request.messages)
+    return {"content": content, "arguments": arguments}
